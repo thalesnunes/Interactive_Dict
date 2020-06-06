468a20619f98f1339ea9a232ab70f7385edfff4d
Thales Nunes <thalesaknunes22@gmail.com>
2020-06-06 00:49:58 -0300

    Finished dict

---
+++ int_dict.py
@@ -0,0 +1,57 @@
+from ferramentas import header
+import json
+import difflib
+
+def load_data():
+    
+    return json.load(open('data.json'))
+
+def get_word(word):
+    
+    return data[word]
+
+def user_interaction():
+    
+    while True:
+        word = str(input('Choose a word to look for: ')).lower()
+        if word == 'q':
+            return False
+        elif word.isalpha():
+            break
+        else:
+            print('Error! Type in a word only with letters!')
+    
+    return word
+
+def print_defs(word):
+    
+    for i, definition in enumerate(get_word(word)):
+                print(f'{i+1}. {definition}')
+                
+
+if __name__ == "__main__":
+    
+    header('Interactive English Dictionary')
+    
+    data = load_data()
+    
+    while True:
+        print('Type "q" to end the programm')
+        
+        word = user_interaction()
+        if not word:
+            print('See you soon...')
+            break
+                
+        try:
+            print_defs(word)
+        except:
+            match = difflib.get_close_matches(word, list(data.keys()),
+                                              n=2, cutoff=0.8)
+            for guess in match:
+                choice = str(input(f'Do you mean {guess}?(Y/N): ')).lower()
+                if choice == 'y':
+                    print_defs(guess)
+                    break
+                else:
+                    continue    
---
+++ test.py
@@ -0,0 +1,4 @@
+dict1 = {'rain': [1, 2], 'shoe':[3, 4]}
+
+if 'rai' in dict1:
+    print('yes')
